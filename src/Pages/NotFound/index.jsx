0f315c48b72d8a09f9f414163194ca8959fa7061
juanjomayotte <juanjomayotte@gmail.com>
2023-12-04 15:08:46 +0100

    Modifique el not found

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -4,7 +4,13 @@ function NotFound() {
     return (
       <>
         <Layout>
-        NotFound
+          <h1>Welcome to</h1>
+          <img
+              src="https://i.imgur.com/VzoEBDJ.png"
+              className="w-52"
+              alt="LIME Shop"
+            />
+            <p>Sign In or Register to access</p>
         </Layout>
       </>
     )
